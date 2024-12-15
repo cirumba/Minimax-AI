9717a8bd7dfbecb2acdf8c04eca5f2f161d77faa
Guilherme Cirumbolo <gcirumbolo@gmail.com>
2024-12-15 14:30:01 -0300

    othello count DONE

--- src/advsearch/your_agent/othello_minimax_count.py
+++ src/advsearch/your_agent/othello_minimax_count.py
@@ -17,17 +17,19 @@ def make_move(state) -> Tuple[int, int]:
 
 
 def evaluate_count(state, player:str) -> float:
-    # Se o estado for terminal, retorne a utilidade do estado
-    if state.is_terminal():
-        winner = state.winner()
+    
+    if state.board.is_terminal_state():
+        winner = state.board.winner()
         if winner == player:
-            return float('inf')  # Vitória para o jogador atual
-        elif winner == state.opponent(player):
-            return float('-inf')  # Vitória para o oponente
-        else:
+            return 31  # Vitória para o jogador atual
+        elif winner is None:
             return 0  # Empate
+        else:
+            return -31  # Vitória para o oponente
+        
+    opponent = Board.opponent(player)
+    # Contar peças diretamente no tabuleiro
+    player_count = state.board.num_pieces(player)
+    opponent_count = state.board.num_pieces(opponent)
 
-    # Caso não seja terminal, calcule a diferença de peças
-    player_count = state.count_pieces(player)
-    opponent_count = state.count_pieces(state.opponent(player))
     return player_count - opponent_count
