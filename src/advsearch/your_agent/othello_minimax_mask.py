c2ae574c59a7a4c5b779396065a259f0bb926c57
Guilherme Cirumbolo <gcirumbolo@gmail.com>
2024-12-15 14:30:09 -0300

    othello mask DONE

--- src/advsearch/your_agent/othello_minimax_mask.py
+++ src/advsearch/your_agent/othello_minimax_mask.py
@@ -26,27 +26,21 @@ EVAL_TEMPLATE = [
 
 
 def make_move(state) -> Tuple[int, int]:
-    """
-    Returns a move for the given game state
-    :param state: state to make the move
-    :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
-    """
-
-    # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada 
-    # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
-    # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
-
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    max_depth = 5  
+    return minimax_move(state, max_depth=max_depth, eval_func=evaluate_mask)
 
 
-def evaluate_mask(state, player:str) -> float:
-    """
-    Evaluates an othello state from the point of view of the given player. 
-    If the state is terminal, returns its utility. 
-    If non-terminal, returns an estimate of its value based on the positional value of the pieces.
-    You must use the EVAL_TEMPLATE above to compute the positional value of the pieces.
-    :param state: state to evaluate (instance of GameState)
-    :param player: player to evaluate the state for (B or W)
-    """
-    return 0   # substitua pelo seu codigo
+def evaluate_mask(state, player: str) -> float:
+    opponent = Board.opponent(player)
+    player_score = 0
+    opponent_score = 0
+
+    for x in range(8):
+        for y in range(8):
+            piece = state.board.tiles[y][x]
+            if piece == player:  # Se a posição for do jogador
+                player_score += EVAL_TEMPLATE[x][y]
+            elif piece == opponent:  # Se a posição for do oponente
+                opponent_score += EVAL_TEMPLATE[x][y]
+
+    return player_score - opponent_score
